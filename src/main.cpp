68eb912b39828ca3dd3636a16803b987c7c84740
audiencia-cereal <42773532+audiencia-cereal@users.noreply.github.com>
2018-10-16 22:10:17 -0400

    defined introduction method, changed heading

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 /******************************************************************************
   Title          : main.cpp
-  Author         : LiudmilaZyrianova239
+  Author         : audiencia-cereal
   Created on     : October  7, 2018
   Description    : Displays random peculiar facts
   Purpose        : To exercise collaborative source code development
   Usage          : nuttyfact
   Build with     : g++ -Wall -g -o oddities  main.cpp
-  Modifications  : Added output_function  LiudmilaZyrianova239_oddity;
+  Modifications  : Added LiudmilaZyrianova239_oddity to main file
   Notes:
   The number N below is the number of people on each team.
   This program prints N peculiar facts, called "oddities" here.
@@ -35,9 +35,13 @@ using namespace std;
 /******************************************************************************/
 
 // The syntax of the output function that must be used to print an oddity.
-typedef ostream& (*output_function) (const ostream & out);
+typedef ostream& (*output_function) (ostream & out);
 
-
+ostream& audiencia_cereal_oddity(ostream & out)
+{
+    out << "This is a collection of strange but true facts.\n";
+    return out;
+}
 ostream& LiudmilaZyrianova239_oddity(ostream & out)
 {
     out << "If 7-11 is open 24 hours a day, 365 days a year, why are there locks on the doors?\n";
@@ -46,7 +50,14 @@ ostream& LiudmilaZyrianova239_oddity(ostream & out)
 
 int main(int argc, char* argv[] )
 {
-    LiudmilaZyrianova239_oddity(cout);
+    //Create an object of type output_function
+    //supply std::cout as an argument to object, print statement
+    output_function audiencia_cereal = &audiencia_cereal_oddity;
+    audiencia_cereal(cout); 
+
+    //Call function by LiudmilaZyrianova239
+    output_function LiudmilaZyrianova239 = &LiudmilaZyrianova239_oddity;
+    LiudmilaZyrianova239(cout);
 
     return 0;
 }
